2ba04815b6a0ddc5cdd91503a893a494b25c0342
shokred <shokred@yandex.ru>
2019-05-23 22:07:10 +0700

    Add spring framework

---
+++ src/main/kotlin/name/shokred/demo/kafka/KafkaApplication.kt
@@ -0,0 +1,11 @@
+package name.shokred.demo.kafka
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KafkaApplication
+
+fun main(args: Array<String>) {
+	runApplication<KafkaApplication>(*args)
+}
